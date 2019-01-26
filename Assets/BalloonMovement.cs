a112ef5af7806c53067966a0a2f64c151c1f642e
maria s <maria.sumedre@gmail.com>
2019-01-27 00:56:31 +0200

    ballon has its own script, change scene to fit gameplay

---
+++ Assets/BalloonMovement.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class BalloonMovement : MonoBehaviour
+{
+    [SerializeField] float rotationWhenFlying = -45;
+    [SerializeField] float rotationWhenWalking = 15;
+    [SerializeField] float horizontalModificator = 2;
+    [SerializeField] float ropeLength = 0.5f;
+    [SerializeField] float balloonHelium = 1f;
+
+    bool tiedToPlayer = true;
+
+    Vector3 origin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        origin = transform.localPosition;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("balloon collided with: " + collision.gameObject.tag);
+        switch (collision.gameObject.tag)
+        {
+            case "unlock":
+                print("Finish unlocked");
+                //Destroy(collision.gameObject); //bye bye mushroom
+                collision.gameObject.transform.localScale = new Vector3(0f, 0f, 0f);
+                transform.parent.gameObject.SendMessage("UnlockFinish");
+                break;
+            default:
+                break;
+        }
+    }
+        // Update is called once per frame
+    void Update()
+    {
+        float horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
+        float horizontalChange = horizontal * horizontalModificator * Time.deltaTime;
+        bool isFiring = CrossPlatformInputManager.GetButton("Fire1");
+        bool isFlying = transform.parent.localPosition.y > -3.2;
+
+        if (isFiring)
+        {
+            print("release the balloon");
+            tiedToPlayer = false;
+        }
+        if (tiedToPlayer)
+        {
+            if (isFlying)
+            {
+                transform.rotation = Quaternion.Euler(0, 0, horizontal * rotationWhenFlying);
+                float xpos = transform.localPosition.x + horizontalChange;
+                float range = ropeLength + origin.x;
+                float horizontalShift = Mathf.Clamp(xpos, -range, range);
+                transform.localPosition = new Vector3(
+                    horizontalShift,
+                    origin.y,
+                    origin.z);
+            }
+            else
+            {
+                transform.rotation = Quaternion.Euler(0, 0,  horizontal * rotationWhenWalking);
+                transform.localPosition = new Vector3(
+                    Mathf.Clamp(transform.localPosition.x, -origin.x, origin.x),
+                    Mathf.Clamp(transform.localPosition.y, -origin.y, origin.y),
+                    origin.z);
+            }
+        }
+        else
+        {
+            transform.localPosition = new Vector3(
+                transform.localPosition.x,
+                transform.localPosition.y + balloonHelium * Time.deltaTime,
+                transform.localPosition.z
+                );
+            transform.localRotation = Quaternion.Euler(new Vector3(0f, 0f, 10f * Mathf.Sin(10 * Time.time)));
+            if (transform.localPosition.y > 7f)
+            {
+                print("balloon destroyed");
+                transform.localScale = new Vector3(0f, 0f, 0f); //poof
+            }
+        }
+    }
+}
--- Assets/KidMovement.cs
+++ Assets/KidMovement.cs
@@ -7,21 +7,19 @@ using UnityEngine.SceneManagement;
 public class KidMovement : MonoBehaviour
 {
     [SerializeField] float horizontalModificator = 5;
-    [SerializeField] float balloonRotation = 100;
-    [SerializeField] float balloonHelium = 1f;
     [SerializeField] float verticalModificator = 5;
     [SerializeField] int   blinkPause = 15;
 
     enum Animations {Jump, Fall, Run, Blink, None};
     Animations playing;
-    bool hasBalloonControl = true;
     bool playerLock = false;
 
     Rigidbody2D rigidBody = null;
-    Transform balloonTransform = null;
+    SpriteRenderer kidRenderer = null;
     Animator animator = null;
-    Vector3 balloonOrigin;
     GameObject winMessage = null;
+
+    public bool finishLocked = true;
 
     // Start is called before the first frame update
     void Start()
@@ -29,8 +27,7 @@ public class KidMovement : MonoBehaviour
         print("hello!");
         rigidBody = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        balloonTransform = transform.GetChild(0);
-        balloonOrigin = balloonTransform.localPosition;
+        kidRenderer = GetComponent<SpriteRenderer>();
         playing = Animations.Blink;
         winMessage = GameObject.Find("Win Message");
     }
@@ -60,16 +57,19 @@ public class KidMovement : MonoBehaviour
                 Invoke("LoadFirstLevel", 0.75f);
                 break;
             case "Finish":
-                print("You won");
-                playerLock = true;
-                animator.Play("kid idle");
-                animator.speed = 0;
-                winMessage.transform.localPosition = new Vector3(
-                    winMessage.transform.localPosition.x,
-                    0.25f, //winMessage.transform.localPosition.y ,
-                    winMessage.transform.localPosition.z);
-                // how to print message on screen?
-                Invoke("LoadFirstLevel", 5f);
+                if (!finishLocked)
+                {
+                    print("You won");
+                    playerLock = true;
+                    animator.Play("kid idle");
+                    animator.speed = 0;
+                    winMessage.transform.localPosition = new Vector3(
+                        winMessage.transform.localPosition.x,
+                        0.27f,
+                        winMessage.transform.localPosition.z);
+                    // how to print message on screen?
+                    Invoke("Quit", 3f);
+                }
                 break;
             default:
                 break;
@@ -84,34 +84,13 @@ public class KidMovement : MonoBehaviour
     private void MovementOnInput()
     {
         float horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
+        kidRenderer.flipX = horizontal < 0;
+
         float horizontalChange = horizontal * horizontalModificator * Time.deltaTime;
         transform.localPosition = new Vector3(
             transform.localPosition.x + horizontalChange,
             transform.localPosition.y,
             transform.localPosition.z);
-        if (hasBalloonControl)
-        {
-            if (transform.localPosition.y > -3.2)
-            {
-                balloonTransform.rotation = Quaternion.Euler(0, 0, horizontal * balloonRotation);
-                float xpos = balloonTransform.localPosition.x + horizontalChange;
-                float range = 0.5f + balloonOrigin.x;
-                float balloonHorizontalShift = Mathf.Clamp(xpos, -range, range);
-                balloonTransform.localPosition = new Vector3(
-                    balloonHorizontalShift,
-                    balloonOrigin.y,
-                    balloonOrigin.z);
-            }
-            else
-            {
-                balloonTransform.rotation = Quaternion.Euler(0, 0, 0);
-                balloonTransform.localPosition = new Vector3(
-                    Mathf.Clamp(balloonTransform.localPosition.x, -balloonOrigin.x, balloonOrigin.x),
-                    Mathf.Clamp(balloonTransform.localPosition.y, -balloonOrigin.y, balloonOrigin.y),
-                    balloonOrigin.z);
-            }
-        }
-
         bool isJumping = CrossPlatformInputManager.GetButton("Jump");
         if (isJumping)
         {
@@ -119,27 +98,6 @@ public class KidMovement : MonoBehaviour
             rigidBody.freezeRotation = true;
             rigidBody.AddRelativeForce(Vector3.up * jump);
             rigidBody.freezeRotation = false;
-        }
-
-        bool isFiring = CrossPlatformInputManager.GetButton("Fire1");
-        if (isFiring)
-        {
-            print("release the balloon");
-            hasBalloonControl = false;
-        }
-        if (!hasBalloonControl)
-        {
-            balloonTransform.localPosition = new Vector3(
-                balloonTransform.localPosition.x,
-                balloonTransform.localPosition.y + balloonHelium * Time.deltaTime,
-                balloonTransform.localPosition.z
-                );
-            balloonTransform.localRotation = Quaternion.Euler(new Vector3(0f, 0f, 10f * Mathf.Sin(10*Time.time)));
-            if (balloonTransform.localPosition.y > 7f)
-            {
-                print("balloon destroyed");
-                balloonTransform.localScale = new Vector3(0f, 0f, 0f); //poof
-            }
         }
         Animate(horizontal, isJumping);
     }
@@ -194,5 +152,16 @@ public class KidMovement : MonoBehaviour
     {
         playing = Animations.None;
         animator.speed = 0;
+    }
+
+    public void UnlockFinish()
+    {
+        print("got message from balloon");
+        finishLocked = false;
+    }
+
+    private void Quit()
+    {
+        Application.Quit();
     }
 }
