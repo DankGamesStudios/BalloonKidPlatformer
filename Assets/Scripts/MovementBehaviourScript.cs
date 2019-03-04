8d986b490973928c5b772d11b3bbb1b65728a9a7
Ana Maria Totorean <anamaria.totorean@3pillarglobal.com>
2019-03-04 17:13:30 +0200

    BKP-1: Animate walking

---
+++ Assets/Scripts/MovementBehaviourScript.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementBehaviourScript : MonoBehaviour
+{
+    public float maxSpeed = 10.0f;
+    bool facingRight = true;
+
+    Animator anim;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float move = Input.GetAxis("Horizontal");
+        anim.SetFloat("Speed", Mathf.Abs(move));
+        rb.velocity = new Vector2(rb.velocity.x + move * maxSpeed, rb.velocity.y);
+
+
+        if (move > 0 && !facingRight)
+        {
+            FlipFacing();
+        }
+        else if(move < 0 && facingRight)
+        {
+            FlipFacing();
+        }
+    }
+
+    void FlipFacing()
+    {
+        facingRight = !facingRight;
+        Vector3 characterScale = transform.localScale;
+        characterScale.x *= -1;
+        transform.localScale = characterScale;
+    }
+}
+
+
